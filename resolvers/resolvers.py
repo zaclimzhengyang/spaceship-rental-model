7b37972241b3a3826ea5da86ade594913b068aa0
limzhengyang <zhengyanglim057@gmail.com>
2023-05-17 14:26:06 +0800

    git commit "return the optimal list of contracts"

---
+++ resolvers/resolvers.py
@@ -0,0 +1,12 @@
+from models.contract import Contract
+from models.slim_solution import SlimSolution
+from services.definite_solutions.definite_contract_selector_service import DefiniteContractSelectorService
+
+DEFINITE_CONTRACT_SELECTOR_SERVICE: DefiniteContractSelectorService = DefiniteContractSelectorService()
+
+
+def resolve_optimize_contracts(contracts: list[Contract]) -> SlimSolution:
+    """
+    given a list of contracts, return the optimal ones
+    """
+    return DEFINITE_CONTRACT_SELECTOR_SERVICE.select_contracts(contracts).to_slim()
